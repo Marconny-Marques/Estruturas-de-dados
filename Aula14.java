d143e830334b943aca088e6443a02c13002f55b4
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-11-07 14:18:02 -0300

    primeiros passos em Pilhas

---
+++ Aula14.java
@@ -0,0 +1,10 @@
+public class Aula14 {
+    
+    Pilha<Integer> pilha = new Pilha<Integer>();
+
+    for (int i = 1; i<=10; i++) {
+        pilha.empilha(i);
+    }
+    system.out.println(pilha);
+    system.out.println(pilha.tamanho());
+}
--- Vetor.labs/exer02.java
+++ Vetor.labs/exer02.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-
-public class exer02 {
+    
+public class exer02 {           
     public static void main(String[] args) {
         
         ArrayList<String> arrayList = new ArrayList<String>(5);
