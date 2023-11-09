6da9ac36a49ec6ad0f6f8ece5152e11298303db9
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-11-09 17:28:04 -0300

    add more steps about Data Structure

---
+++ Exerlabs/Livro.java
@@ -0,0 +1,50 @@
+package Exerlabs;
+
+public class Livro {
+    private String isbn;
+    private String autor;
+    private int anolançamento;
+    private String nome;
+
+    public Livro() {
+        super();
+    }
+
+    public Livro(String isbn, String autor, int anolançamento, String nome) {
+        this.isbn = isbn;
+        this.autor = autor;
+        this.anolançamento = anolançamento;
+        this.nome = nome;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public int getAnolançamento() {
+        return anolançamento;
+    }
+    public void setAnolançamento(int anolançamento) {
+        this.anolançamento = anolançamento;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro [isbn=" + isbn + ", autor=" + autor + ", anolançamento=" + anolançamento + ", nome=" + nome + "]";
+    }
+
+    
+}
---
+++ Exerlabs/Pilha.java
@@ -0,0 +1,27 @@
+package Exerlabs;
+
+public class Pilha<T> {
+
+    public void empilha(Livro livro1) {
+    }
+
+    public Integer desempilha() {
+        return null;
+    }
+
+    public boolean estaVazia() {
+        return false;
+    }
+
+    public void empilha(int num) {
+    }
+
+    public String tamanho() {
+        return null;
+    }
+
+	public String topo() {
+		return null;
+	}
+
+}
