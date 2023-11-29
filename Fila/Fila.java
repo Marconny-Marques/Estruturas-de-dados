106683620fdb604a7e4e01cde09c1d14eefaa1aa
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2023-11-29 17:52:49 -0300

    Conceito de fila em java

---
+++ Fila/Fila.java
@@ -0,0 +1,27 @@
+package Fila;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public void enfileira(T elemento) {
+        //this.elementos[this.tamanho] = elemento;
+        //this.tamanho++;
+
+        this.adiciona(elemento);
+    }
+
+    public T espiar() {
+
+        if (this.estaVazia()) {
+            return null;
+        }
+
+        return this.elementos[0];
+    }
+}
