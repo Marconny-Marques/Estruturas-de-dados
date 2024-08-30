9fd3a8020deb79ca0d5278a6b0f6125ee2637d43
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2024-08-30 17:41:54 -0300

    knowledges about structure data

---
+++ estruturadados/aula2.java
@@ -0,0 +1,7 @@
+package estruturadados;
+
+public class aula2 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(100);
+    }
+}
---
+++ estruturadados/aula3.java
@@ -0,0 +1,8 @@
+package estruturadados;
+
+public class aula3 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        vetor.adiciona("elemento 1");
+    }
+}
